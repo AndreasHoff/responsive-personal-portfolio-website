b1eb03da35d6f83561a488c1bf46daaf99fe81a4
AndreasHoff <mchoffn@hotmail.com>
2023-04-16 23:45:45 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -41,3 +41,22 @@ function addNavbarBorderOnScroll() {
   }
   
   window.addEventListener('scroll', addNavbarBorderOnScroll);
+
+  ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
